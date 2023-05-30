5d6c8ba66a71dd70dbcfe9a2fe36244a88e5ef16
DevFabrizio <rockout94@hotmail.it>
2023-05-30 06:41:02 +0000

    add update functionality for tasks

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
     //   datepicker element
-    let datepicker = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
-        format: 'dd mmmm, yyyy',
-        i18n: {done: 'Select'}
-
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" },
     });
     // select initialization
-    let selects = document.querySelectorAll('select');
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
-});
 
+    // collapsibile element initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
+    
+});
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route('/categories')
@@ -47,7 +48,7 @@ def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == 'POST':
         task = Task(
-            task_name=request.form.get('task_name'), 
+            task_name=request.form.get('task_name'),
             task_description=request.form.get('task_description'),
             is_urgent=bool(True if request.form.get('is_urgent') else False),
             due_date=request.form.get('due_date'),
