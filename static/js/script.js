c3d8e1567d56ca3c30818d7ffe79e8fe5a6b45e3
DevFabrizio <rockout94@hotmail.it>
2023-05-30 08:34:24 +0000

    add full CRUD functionality

--- static/js/script.js
+++ static/js/script.js
@@ -2,18 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-    //   datepicker element
+
+    // datepicker initialization
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
-        i18n: { done: "Select" },
+        i18n: { done: "Select" }
     });
+
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    // collapsibile element initialization
-    let collapsibles = document.querySelectorAll('.collapsible');
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
-    
 });
